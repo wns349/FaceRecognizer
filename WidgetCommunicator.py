71522141006028af897ec0583e2fe40ebfbd722c
Junsung Lim <wns349@kaist.ac.kr>
2014-10-27 17:38:52 +0900

    added delay option

--- WidgetCommunicator.py
+++ WidgetCommunicator.py
@@ -1,14 +1,24 @@
 import logging
+import time
 import py4j.java_gateway
+
+def getTime():
+  return int(round(time.time() * 1000))
 
 class WidgetCommunicator:
   def __init__(self):
     self.widget = None
     self.gateway = None
+    self.delay = 0
+    self.lastsent = 0
+
 
   def initialize(self, configs):
     if(configs['widget.use'] != 'true'):
       return
+
+    # Load delay info
+    self.delay = int(configs['widget.delay'])
 
     # Load client
     address = configs['widget.jvmaddress']
@@ -18,7 +28,6 @@ class WidgetCommunicator:
     # Get class
     self.gateway = py4j.java_gateway.JavaGateway(gateway_client = client)
     contextWidgetFactory = self.gateway.entry_point.getContextWidgetFactory()
-
     # Create widget
     widgetName = configs['widget.widgetname']
     contextName = configs['widget.contextname']
@@ -28,8 +37,11 @@ class WidgetCommunicator:
     self.widget = contextWidgetFactory.createFaceRecognitionContextWidget(widgetName, contextName, contextType)
 
   def report(self, predictResult):
-    if (not self.widget is None):
+    if (not self.widget is None and getTime() - self.lastsent >= self.delay):
+      logging.info('Widget Reported')
       self.widget.callback(predictResult[0])
+      self.lastsent = getTime()
+
 
   def terminate(self):
     logging.info('Terminating WidgetCommunicator...')
